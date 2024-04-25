c7b705a7cd41a8df6d1266e502a4706c9df66920
Kaustubh-Dengle <dengle.kaustubh@gmail.com>
2024-04-25 19:06:01 +0530

    Changes added to webpage

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -2,33 +2,77 @@ import Header from "./components/Header";
 import CoreConcept from "./components/CoreConcept";
 import { CORE_CONCEPTS } from "./data";
 import TabButton from "./components/TabButton";
+import { EXAMPLES } from "./data";
+import { useState } from "react";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header />
       <main>
-        <section id = "core-concepts">
+        <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept //this is a lengthier way of showing data on screen
-              title = {CORE_CONCEPTS[0].title} 
-              description = {CORE_CONCEPTS[0].description}
-              image = {CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]}  /**use spread operator here to pull out contents of CORE_CONCEPTS and display key value pairs*/ />
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key = {conceptItem.title} {...conceptItem} />)}
+            {/* <CoreConcept //this is a lengthier way of showing data on screen
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            /> */}
+            {/* <CoreConcept
+              {...CORE_CONCEPTS[1]} use spread operator here to pull out contents of CORE_CONCEPTS and display key value pairs
+            /> */}
+            {/* <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />  */}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
